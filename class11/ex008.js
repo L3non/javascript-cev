bee0624ce59a80a43a85addd104b8fd2b2de53e9
Lenon <fernandesbruno01384@gmail.com>
2024-02-13 00:41:45 -0300

    exercises 006 to 011

---
+++ class11/ex008.js
@@ -0,0 +1,6 @@
+var s = 60.5
+console.log(`Your car speed is ${s}Km/h`)
+if (s > 60) {
+    console.log(`You exceeded the permitted speed. FINED!`)
+}
+console.log(`Always drive using life belt!`)
---
+++ class11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Brasil'
+console.log(`Living in ${country}`)
+if (country == 'Brasil') {
+    console.log(`You are Brazilian!`)
+} else {
+    console.log(`You are a foreigner!`)
+}
